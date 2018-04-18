48a48a17d815829cd92a82360e2b06022cc2b058
ahmad-choirul <31815400+ahmad-choirul@users.noreply.github.com>
2018-04-18 22:37:38 +0700

    up game awal
    a

---
+++ game scm/src/game/scm/GameScm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.scm;
+
+import view.viewawal;
+
+/**
+ *
+ * @author acer
+ */
+public class GameScm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        viewawal a = new viewawal();
+        a.setVisible(true);
+    }
+    
+}
